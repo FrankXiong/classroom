d4ea12aa213c1ed79d057af08de4e4b27f933a84
FrankXiong <1131076966@qq.com>
2016-06-19 01:14:25 +0800

    add CI

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -31,11 +31,7 @@ exports.callback = function(req,res){
     console.log("-------weixin callback---------")
 
     var code = req.query.code
-    // var User = req.model.UserModel
-
     client.getAccessToken(code, function (err, result) {
-        console.dir(err)
-        console.dir(result)
         var accessToken = result.data.access_token;
         var openid = result.data.openid;
         
